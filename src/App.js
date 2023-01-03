1968fb243188af100bcece51dfc5137e5b70c46f
AdithyaVenkatesh <adithyajobs2013@gmail.com>
2023-01-03 01:01:25 -0600

    Header Component Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from "react";
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div><p>First push</p></div>
-  );
+   <Header></Header>);
 }
+
+
 
 export default App;
