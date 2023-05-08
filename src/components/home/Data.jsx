c75a679c540a1816691e0c1dd52c77d09353f297
AdithyaVenkatesh <adithyajobs2013@gmail.com>
2023-05-08 16:11:30 -0500

    test commit

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">
+        Adithya Venkatesh
+
+      </h1>
+      <h3 className="home__subtitle">Master's Student</h3>
+      <p className="home__description">
+      Master student specializing in Computer Science, offering more than 3 years of experience working in fast-paced environments. Skilled in
+developing product with quality and efficiency. Expertise in problem-solving. Skeptic and Curious in learning new stuffs.
+      </p>
+
+      <a href="#contact" className="button button--flex">
+        Contact Me
+        
+      </a>
+    </div>
+  );
+};
+
+export default Data;
